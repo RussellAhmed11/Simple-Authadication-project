aa0eb0d218ca34da38a9f1a42fd4679e39782ac2
RussellAhmed11 <you@example.comahmedrussell437@gmail.com>
2025-07-30 10:54:10 -0600

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <div>
+            <h1>this is heder</h1>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './components/layouts/Main';
+import Home from './components/Home/home';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children:[
+      {
+        path:'/',element:<Home></Home>
+      }
+    ]
+  },
+]);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+     <RouterProvider router={router} />
+  </StrictMode>,
+)
